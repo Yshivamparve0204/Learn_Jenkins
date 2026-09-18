fc26c10365a85a3ac4c79812947c666d413be4dc
Shivam Parve <shivamparve15@gmail.com>
2026-09-18 18:47:22 +0530

    Added files

--- ansible/files/getIP.sh
+++ ansible/files/getIP.sh
@@ -3,6 +3,17 @@ echo $1-$2-$3-$4
 serverIP=`hostname -I`
 hostName=`hostname`
 echo $hostName - $serverIP
-cd software/tomcat10/bin/
+echo "tomcat is starting..."
+cd /home/shivam/software/tomcat-10/bin
 sh startup.sh
 echo "tomcat started"
+sleep 5
+
+if ss -lnt | grep -q ":8080 "; then
+    echo "Tomcat started successfully"
+    echo "Opening Tomcat Home Page..."
+     echo "tomcat_url : http://$serverIP:8080"
+else
+    echo "Tomcat failed to start"
+    exit 1
+fi
